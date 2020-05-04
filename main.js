4f4f4cd359a0abe27b55995952bf3b9c8a3c733d
Adriana Capińska <jablonska.ad@gmail.com>
2020-05-04 14:17:15 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+alert('SIEMA!!');
+console.log('WTF');
+console.log('siema');
+
+const name = 'Adriana';
+const age = '29';
+
+console.log(name);
+console.log(age);
+
+console.log(`Nazywam się ${name} i mam ${age} lat.`)
